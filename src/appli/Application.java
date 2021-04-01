d3c0eda7af345209656403fa1b19ba9100e8d750
Braxoia <ibrahime.ahbib@etu.u-paris.fr>
2021-04-01 16:27:31 +0200

    Mis en place des classes

---
+++ src/appli/Application.java
@@ -0,0 +1,15 @@
+package appli;
+
+public class Application {
+
+    public static void main(String[] args) {
+	    /*  TODO : On demande à l'utilisateur si c'est mode IA, mode Joueur VS IA ou mode 2 joueurs, Création du plateau
+	    *    Si 2 joueurs : scanner pour les 2 comme le projet BPO Le Duel
+	    *       Si joueur ne peut pas jouer sans créer une situation d'echec = echec et pat
+	    *       Sinon l'obligé à jouer
+	    *    Si 1 joueur : scanner uniquement pour le joueur, boucle simplifié car on a juste à étudier un joueur
+	    *    Si 2 IA :  pas de scanner, tout est géré directement dans notre algo et on gère l'affichag
+	    */
+
+    }
+}
