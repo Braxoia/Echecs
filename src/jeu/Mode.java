9365882cdb4e3af944c69ad09b7796d7ebd0d4f3
Braxoia <71882015+Braxoia@users.noreply.github.com>
2021-05-26 00:13:53 +0200

    Ajout de la classe énumérée Mode

---
+++ src/jeu/Mode.java
@@ -0,0 +1,11 @@
+package jeu;
+
+/**
+ * @author A.Ibrahime, B.Marco, P.Julie
+ * @date 25/05/21
+ * @brief Classe Enum Mode
+ */
+public enum Mode {
+    //Les differents mode de jeu
+    OUVERTURE, MILLIEU, FINALE
+}
